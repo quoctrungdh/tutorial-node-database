8242b5ef571a3783326e3ba6951f2acf8f6f4bf1
TrungDo <trungdo@TrungDos-MacBook-Pro.local>
2019-02-27 07:12:56 +0700

    hashing password

--- store.js
+++ store.js
@@ -1,12 +1,33 @@
 const knex = require('knex')(require('./knexfile'))
+const crypto = require('crypto');
+
+function randomString() {
+    return crypto.randomBytes(4).toString('hex')
+}
+
+function saltHashPassword(password) {
+    const salt = randomString();
+    const hash = crypto
+        .createHmac('sha512', salt)
+        .update(password);
+
+    return {
+        salt,
+        hash: hash.digest('hex')
+    }
+}
 
 module.exports = {
+    saltHashPassword,
     createUser ({ username, password }) {
-        console.log(`Creating user ${username} with password ${password}`)
+        console.log(`Creating user ${username}`)
+
+        const { salt, hash } = saltHashPassword(password)
 
         return knex('user').insert({
+            salt,
+            encrypted_password: hash,
             username,
-            password
         })
     }
 }
